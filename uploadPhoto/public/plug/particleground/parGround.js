bd04625d2cb216513017aab15b2e5e0ad7202efe
pikachuWorld <jinqingemail@163.com>
2020-04-26 14:26:13 +0800

    表单简单验证

--- uploadPhoto/public/plug/particleground/parGround.js
+++ uploadPhoto/public/plug/particleground/parGround.js
@@ -9,4 +9,70 @@ $(document).ready(function(){
      lineColor: '#e8ebeb',
      height:  $('.main').height()+100
    });
- });
+  //  function submitCheck(){
+  //     $('#uploadForm input').each(function(key, item){
+      
+  //       let currVal = $(this).val()
+  //       let curTip = $(this).attr('txt')
+    
+  //       if(!currVal){
+  //         console.log(999, $(item.name+'Error'))
+  //         $('#'+item.name+'Error').css('display', 'block')
+  //         $('#'+item.name+'Error').text(curTip)
+  //         return false;
+  //       } else {
+  //       $('#'+item.name+'Error').css('display', 'none')
+  //       $('#'+item.name+'Error').text(curTip)
+  //         return true;
+  //       }
+  //   })
+
+  //  }
+  function valid(){
+    let isBool;
+    $('#uploadForm input.validate').each(function(key, item){
+   
+       let currVal = $(this).val()
+       let curTip = $(this).attr('txt')
+       
+       if(!currVal){
+        
+         $('#'+item.name+'Error').css('display', 'block')
+         $('#'+item.name+'Error').text(curTip)
+         isBool = false;
+         return isBool
+       } else {
+        $('#'+item.name+'Error').css('display', 'none')
+        $('#'+item.name+'Error').text(curTip)
+        isBool = true;
+        return isBool
+       }
+    })
+    return isBool;
+  } 
+  
+
+  //  $('#uploadForm').submit(function(e){
+  //    e.preventDefault();
+  //     console.log('---->', valid())
+  //     // if(valid()) {
+  //     //   $('#uploadForm').submit()
+  //     // }
+      
+  //  })
+  $('#uploadSubmit').click(function (e) { 
+  
+    e = e||window.event;
+    e.preventDefault();    //阻止默认行为
+    if(valid()) {
+       $('#uploadForm').submit()
+    }
+   
+  });
+  
+
+
+
+});
+
+
